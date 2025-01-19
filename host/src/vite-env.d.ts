69609775cecc5a1d1d47bef71bc8a0c1b598ec24
JuHeonHa <hjhjaueon97@gmail.com>
2025-01-19 18:08:22 +0900

    chore: remote typing

--- host/src/vite-env.d.ts
+++ host/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+declare module "remoteApp/Store" {
+  export { useCountStore } from "remoteApp/Store";
+}
+
+declare module "remoteApp/Button" {
+  export { Button } from "remoteApp/Button";
+}
